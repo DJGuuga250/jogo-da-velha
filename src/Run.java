6486119b1e9e017a7e8dfee4745616ff0dd82355
gustavo <gustavomat@gmail.com>
2019-08-21 15:02:50 -0300

    commit inicial

---
+++ src/Run.java
@@ -0,0 +1,9 @@
+
+
+
+public class Run {
+    public static void main(String[] args) {
+        Tela telaDoJogo = new Tela();
+    }
+    
+}
---
+++ src/Tela.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+
+public class Tela extends JFrame{
+
+    public Tela() {
+        super("Jogo da Velha - Tec. Desenv. de jogos digitais");
+        this.setVisible(false);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setExtendedState(MAXIMIZED_BOTH);
+    }
+    
+    
+}
