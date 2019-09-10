081e38a8780ccf898c6494e5871e2f5da24ed686
gustavo <gustavomat@gmail.com>
2019-09-10 17:06:06 -0300

    geração-de-filhos

--- src/Vertice.java
+++ src/Vertice.java
@@ -1,13 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author ITEGO
- */
+import java.util.HashSet;
+import java.util.Set;
+
 public class Vertice {
     
+
+    private Tabuleiro tabuleiroEstado;
+
+    private Set<Vertice> verticesAdjascentes = new HashSet<Vertice>();
+    
+    private Vertice verticePai;
+    
+    private String jogadaText; //saber a jogada é x ou o
+    
+    public String getJogadaText(){
+        return jogadaText;
+    }
+    public void setJogadaText(String jogadaText) {
+        this.jogadaText = jogadaText;
+    }
+    
+    public Vertice getVerticePai() {
+        return verticePai;
+    }
+    public void setVerticePai (Vertice verticePai) {
+        this.verticePai = verticePai;
+    }
+    
+    public Tabuleiro getTabuleiroEstado() {
+        return tabuleiroEstado;
+        
+    }
+    public void setTabuleiroEstado(Tabuleiro TabuleiroEstado){
+        this.tabuleiroEstado = tabuleiroEstado;
+    }
+//add vertices filhos a vertice atual
+    public void adicionarVertice(Vertice vertice) {
+        verticesAdjascentes.add(vertice);
+    }
 }
